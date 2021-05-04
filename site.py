fd5d7903274eaf901ced986a152eb5575f58f257
Denis <denislucasvalverdesilva@gmail.com>
2021-05-04 11:26:44 -0400

    site crud finised

--- site.py
+++ site.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -36,32 +36,105 @@ def aboutMe():
     return render_template("aboutme.html")
 
 
-@app.route('/projects/devlogs')
-def projectsD():
-    return render_template("projectsD.html")
+@app.route('/projects', methods=["POST","GET"])
+def projects():
+    Apost = posts.query.filter_by(game=False)
+    if request.method == "GET":
+        if "id" in request.args: 
+            post = posts.query.get(request.args.get("id"))
+            return request.args.get("id")
+            
+    return render_template("projects.html", values=Apost)
 
-@app.route('/projects/tutorials')
-def projectsT():
-    return render_template("projectsT.html")
-
-
-@app.route('/games/devlogs')
-def gamesD():
-    return render_template("gamesD.html")
-
-@app.route('/games/tutorials')
-def gamesT():
-    return render_template("gamesT.html")
-
-@app.route('/games/downloads')
+@app.route('/games')
 def games():
     return render_template("games.html")
 
 
-@app.route('/Admin')
+
+@app.route('/admin', methods=["POST","GET"])
 def Admin():
-    return render_template("Admin.html")
+    if request.method == "POST":
+        if "crud" in request.form:
+            if request.form['crud'] == "create":
+                create = True
+                return render_template("admin.html", create=create)
+            if request.form['crud'] == "update":
+                update = True
+                Pst = posts.query.all()
+                return render_template("admin.html", values=Pst, update=update)
+            if request.form['crud'] == "delete":
+                delete = True
+                Pst = posts.query.all()
+                return render_template("admin.html", values=Pst, delete=delete)
+        
+        if "create" in request.form:
+            title = request.form["title"]
+            cont = request.form["content"]
+            if "Game" in request.form:
+                game = True
+            else:
+                game = False
+            if "devlogs" in request.form:
+                dev = True
+            else:
+                dev = False
+            if "Tutorial" in request.form:
+                tut = True
+            else:
+                tut = False
+
+            post = posts(title,cont,game,tut,dev)
+            dbCreate(post)
+        
+        
+        if "delete" in request.form:
+            post = posts.query.get(request.form["delete"])
+            dbDelete(post)
+
+        
+        if "update" in request.form:
+            post = posts.query.get(request.form["update"])
+            return render_template("admin.html", values=post, upd=True)
+
+        if "edit" in request.form:
+            post = posts.query.get(request.form["edit"])
+            
+            title = request.form["title"]
+            cont = request.form["content"]
+            if "Game" in request.form:
+                game = True
+            else:
+                game = False
+            if "devlogs" in request.form:
+                dev = True
+            else:
+                dev = False
+            if "Tutorial" in request.form:
+                tut = True
+            else:
+                tut = False
+            edit = posts(title,cont,game,dev,tut)
+            dbUpdate(edit,post)
+    return render_template("admin.html")
+
+def dbCreate(iten):
+    db.session.add(iten)
+    db.session.commit()
+
+def dbDelete(iten):
+    db.session.delete(iten)
+    db.session.commit()
+
+def dbUpdate(iten,post):
+    dbCreate(iten)
+    dbDelete(post)
+
+
+
 
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
+
+
